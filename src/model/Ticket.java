c12277ba05932069b5d9819ab3c508b2f6b5865f
Dagnis-PC <s6_prusis_d@venta.lv>
2018-06-21 19:58:38 +0300

    ticket class

--- src/model/Ticket.java
+++ src/model/Ticket.java
@@ -1,5 +1,48 @@
 package model;
 
 public class Ticket {
-
+	private int id;
+	private String description;
+	private int price;
+	public static int counter = 1000;
+	public int getId() {
+		return id;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		if(priceChecker(price)) {
+			this.price = price;
+		}
+		else {
+			System.out.println("Invalid price");
+		}
+	}
+	public boolean priceChecker(int price) {
+		if(price >= 0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	public Ticket() {
+		counter++;
+		id = counter + 1; 
+		description = "";
+		price = 0;
+	}
+	public Ticket(String description, int price) {
+		counter++;
+		id = counter + 1;
+		setDescription(description);
+		setPrice(price);
+	}
 }
