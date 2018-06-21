5d4067d1792561366823209ba58be3d80d6e3e19
juris <s6_aizeze_j@venta.lv>
2018-06-21 16:56:15 +0300

    added enclosure

---
+++ src/application/EnclosuresController.java
@@ -0,0 +1,72 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
+public class EnclosuresController {
+	
+	@FXML  
+	private Button butLogout;
+	
+	@FXML  
+	private Button butHome;
+	
+	@FXML  
+	private Button butAddEnclosure;
+	
+	@FXML  
+	private Button butRemoveEnclosure;
+	
+	@FXML  
+	private Button butEditEnclosure;
+	
+	@FXML  
+	private Button butMoveAnimal;
+	
+	@FXML  
+	private Button butRemoveAnimal;
+	
+	@FXML  
+	private ListView listEnclosures;
+	
+	@FXML  
+	private ListView listAnimals;
+	
+	public void ShowAnimals(ActionEvent event) {
+			
+	}
+	
+	public void SelectAnimal(ActionEvent event) {
+		
+	}
+	
+	public void Logout(ActionEvent event) {
+		
+	}
+	
+	public void Home(ActionEvent event) {
+			
+	}
+	
+	public void AddEnclosure(ActionEvent event) {
+		
+	}
+	
+	public void RemoveEnclosure(ActionEvent event) {
+		
+	}
+	
+	public void EditEnclosure(ActionEvent event) {
+		
+	}
+	
+	public void MoveAnimal(ActionEvent event) {
+		
+	}
+	
+	public void RemoveAnimal(ActionEvent event) {
+		
+	}
+}
---
+++ src/application/MoveAnimal.java
@@ -0,0 +1,48 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.text.Text;
+
+public class MoveAnimal {
+	
+	@FXML  
+	private Button butLogout;
+	
+	@FXML  
+	private Button butHome;
+	
+	@FXML  
+	private Button butAccept;
+	
+	@FXML  
+	private Button butCancel;
+	
+	@FXML  
+	private Text txtAnimal;
+	
+	@FXML  
+	private ComboBox Enclosures;
+	
+	public void Home(ActionEvent event) {
+		
+	}
+	
+	public void Logout(ActionEvent event) {
+		
+	}
+	
+	public void Accept(ActionEvent event) {
+		
+	}
+	
+	public void Cancel(ActionEvent event) {
+		
+	}
+	
+	public void SelectEnclosure(ActionEvent event) {
+		
+	}
+}
