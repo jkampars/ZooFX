8656aa878d84108678e80abe7745e48bfd4ba255
Janis Kampars <s6_kampar_j@venta.lv>
2018-06-21 17:20:03 +0300

    Added different mains

---
+++ src/application/MainJuris.java
@@ -0,0 +1,36 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import model.User;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+
+public class MainJuris extends Application {
+	public static User currentUser;
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			//BorderPane root = new BorderPane();
+			Parent root = FXMLLoader.load(getClass().getResource("/Login.fxml"));
+			Scene scene = new Scene(root,700,500);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			//primaryStage.setResizable(false);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		currentUser = new User("login","password");
+		currentUser.setAdmin();
+		launch(args);
+	}
+
+}
