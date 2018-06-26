bbc8a0eac791eedab00a108bcc0b20b7d0f7ebed
juris <s6_aizeze_j@venta.lv>
2018-06-27 01:00:11 +0300

    enclosure polished

--- src/model/Enclosure.java
+++ src/model/Enclosure.java
@@ -8,39 +8,44 @@ public class Enclosure implements Serializable{
 	private int id;
 	private int MAX_CAPACITY;
 	private ArrayList<Animal> animals = new ArrayList<>();
-	//private int current_capacity = animals.size();
 	public static int counter = 1000;
 	private String name;
 	private AnimalType type;
+	
 	public int getMAX_CAPACITY() {
 		return MAX_CAPACITY;
 	}
+	
 	public void setMAX_CAPACITY(int mAX_CAPACITY) {
 		MAX_CAPACITY = mAX_CAPACITY;
 	}
+	
 	public int getCurrent_capacity() {
-		return animals.size()/*current_capacity*/;
+		return animals.size();
 	}
-	/*public void setCurrent_capacity(int current_capacity) {
-		this.current_capacity = current_capacity;
-	}*/
+	
 	public ArrayList<Animal> getAnimals() {
 		return animals;
 	}
+	
 	public void setAnimals(Animal animal) {
 		animals.add(animal);
 	}
+	
 	public String getName() {
 		return name;
 	}
+	
 	public void setName(String _name) {
 		if(checkName(_name)) {
 			name = _name;
 		}
+		
 		else {
 			System.out.println("Invalid name");
 		}
 	}
+	
 	public AnimalType getType() {
 		return type;
 	}
@@ -48,36 +53,39 @@ public class Enclosure implements Serializable{
 	public void setType(AnimalType type) {
 		this.type = type;
 	}
+	
 	public int getId() {
 		return id;
 	}
+	
 	public static boolean checkName(String name) {
 		if (name.matches("[a-zA-Z0-9]*")) 
 			return true;
+		
 		else 
 			return false;
 	}
+	
 	public void addAnimal(Animal animal) {
 		animals.add(animal);
-		//current_capacity++;
 	}
+	
 	public void removeAnimal(int index) {
 		animals.remove(index);
-		//current_capacity--;
 	}
+	
 	public Enclosure() {
 		counter++;
 		id = counter + 1;
 		name = "";
 		MAX_CAPACITY = 0;
-		//current_capacity = 0;
 		type = AnimalType.NONE;
 	}
-	public Enclosure(int mAX_CAPACITY/*, int current_capacity*/, String name, AnimalType type) {
+	
+	public Enclosure(int mAX_CAPACITY, String name, AnimalType type) {
 		counter++;
 		id = counter + 1;
 		setMAX_CAPACITY(mAX_CAPACITY);
-		//setCurrent_capacity(current_capacity);
 		setName(name);
 		setType(type);
 	}
@@ -85,29 +93,35 @@ public class Enclosure implements Serializable{
 	public String toString() {
 		return name;
 	}
+	
 	public boolean animalsIsEmpty() {
 		if(animals.isEmpty()) {
 			return true;
 		}
+		
 		else {
 			return false;
 		}
 	}
+	
 	public boolean isEmpty() {
-		if (animals.size()/*current_capacity*/==0)
+		if (animals.size()==0)
 			return true;
+		
 		else
 			return false;
 	}
+	
 	public void removeAnimal(Animal animal) {
 		if (animals.contains(animal)) {
 			animals.remove(animal);
-			//current_capacity--;
 		}
 	}
+	
 	public boolean isFull() {
-		if (animals.size()/*current_capacity*/==MAX_CAPACITY)
+		if (animals.size()==MAX_CAPACITY)
 			return true;
+		
 		else
 			return false;
 	}
