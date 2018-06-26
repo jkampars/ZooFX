68180287f7da3eda68df1f3588c8b59cec918571
Dagnis-PC <s6_prusis_d@venta.lv>
2018-06-26 21:03:39 +0300

    name check when creating enclosure

--- src/application/AddEnclosureController.java
+++ src/application/AddEnclosureController.java
@@ -8,9 +8,11 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import model.AnimalType;
@@ -48,24 +50,37 @@ public class AddEnclosureController extends Main{
 	}
 	
 	public void Accept(ActionEvent event) throws IOException {
+		boolean correctInput = true;
 		int capacity = Integer.parseInt(txtCapacity.getText());
 		Enclosure buris = new Enclosure();
-		buris.setName(txtName.getText());
-		System.out.println(txtName.getText());
-		buris.setType((AnimalType) selectType.getSelectionModel().getSelectedItem());
-		System.out.println((AnimalType) selectType.getSelectionModel().getSelectedItem());
-		buris.setMAX_CAPACITY(capacity);
-		System.out.println(capacity);
-		enclosures.add(buris);
-		System.out.println("Enclosure has been made");
-		for (Enclosure i : enclosures) {
-			System.out.println(i.getName());
+		if(Enclosure.checkName(txtName.getText())) {
+			buris.setName(txtName.getText());
+			System.out.println(txtName.getText());
+			buris.setType((AnimalType) selectType.getSelectionModel().getSelectedItem());
+			System.out.println((AnimalType) selectType.getSelectionModel().getSelectedItem());
+			buris.setMAX_CAPACITY(capacity);
+			System.out.println(capacity);
+			enclosures.add(buris);
+			System.out.println("Enclosure has been made");
+			for (Enclosure i : enclosures) {
+				System.out.println(i.getName());
+			}
 		}
-		FXMLLoader loader =  new FXMLLoader(getClass().getResource("/Enclosures.fxml"));// create and load() view
-		loader.load();
-		Stage stage = (Stage) butAccept.getScene().getWindow();
-		Scene scene = new Scene(loader.getRoot());
-		stage.setScene(scene);
+		else {
+			Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Enclosure name input fail");
+            alert.setHeaderText("Enclosure name consists only of letters and numbers");
+            alert.show();
+            correctInput = false;
+		}
+		
+		if(correctInput) {
+			FXMLLoader loader =  new FXMLLoader(getClass().getResource("/Enclosures.fxml"));// create and load() view
+			loader.load();
+			Stage stage = (Stage) butAccept.getScene().getWindow();
+			Scene scene = new Scene(loader.getRoot());
+			stage.setScene(scene);
+		}
 		
 	}
 	
